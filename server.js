71bbd76361794719418b733ec2587cc42c0afac2
krnlexecutor735-cell <krnlexecutor735@gmail.com>
2026-07-12 17:28:44 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,26 @@ const io = require('socket.io')(server);
 
 app.use(express.static(__dirname));
 
+let players = {};
+
 io.on('connection', (socket) => {
-    console.log('User connected: ' + socket.id);
+    socket.on('init', (data) => {
+        players[socket.id] = { x: 500, y: 500, os: data.os };
+        io.emit('update', players);
+    });
+
+    socket.on('move', (pos) => {
+        if (players[socket.id]) {
+            players[socket.id].x = pos.x;
+            players[socket.id].y = pos.y;
+            socket.broadcast.emit('move', { id: socket.id, x: pos.x, y: pos.y });
+        }
+    });
+
+    socket.on('disconnect', () => {
+        delete players[socket.id];
+        io.emit('remove', socket.id);
+    });
 });
 
 server.listen(process.env.PORT || 3000);
