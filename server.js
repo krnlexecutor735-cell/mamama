c67ed8bb2682cd6a87f6c1ead082d82d100cd093
krnlexecutor735-cell <krnlexecutor735@gmail.com>
2026-07-12 17:20:13 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(express.static(__dirname));
 let players = {};
 
 io.on('connection', (socket) => {
-    players[socket.id] = { x: 500, y: 500 };
+    // รอรับค่า OS ตอนเชื่อมต่อครั้งแรก
+    socket.on('init', (os) => {
+        players[socket.id] = { x: 500, y: 500, os: os };
+        // ส่งข้อมูลผู้เล่นใหม่ให้คนอื่นในห้อง
+        socket.broadcast.emit('newPlayer', { id: socket.id, os: os, x: 500, y: 500 });
+        // ส่งรายชื่อผู้เล่นทั้งหมดที่มีอยู่แล้วให้คนที่เพิ่งเข้ามา
+        socket.emit('currentPlayers', players);
+    });
+
     socket.on('move', (pos) => {
         if (players[socket.id]) {
             players[socket.id].x = pos.x;
@@ -16,6 +24,7 @@ io.on('connection', (socket) => {
             socket.broadcast.emit('move', { id: socket.id, x: pos.x, y: pos.y });
         }
     });
+
     socket.on('disconnect', () => { delete players[socket.id]; io.emit('remove', socket.id); });
 });
 
