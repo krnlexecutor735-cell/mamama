278906f1a8d8d7ca525395039f915e9eb4164a40
krnlexecutor735-cell <krnlexecutor735@gmail.com>
2026-07-12 17:59:50 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,20 +2,32 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const path = require('path');
+
 app.use(express.static(__dirname));
 
 let players = {};
 
 io.on('connection', (socket) => {
-    players[socket.id] = { x: 0, y: 0, os: socket.handshake.query.os || 'Unknown' };
-    socket.on('move', (pos) => {
+    // กำหนดตำแหน่งเริ่มต้น
+    players[socket.id] = { x: 400, y: 300, os: socket.handshake.query.os || 'Unknown' };
+
+    // รับคำสั่งเคลื่อนที่ (รับมาแค่ทิศทาง dx, dy)
+    socket.on('move', (data) => {
         if (players[socket.id]) {
-            players[socket.id].x = pos.x;
-            players[socket.id].y = pos.y;
+            players[socket.id].x += data.dx;
+            players[socket.id].y += data.dy;
         }
     });
-    socket.on('disconnect', () => delete players[socket.id]);
+
+    socket.on('disconnect', () => {
+        delete players[socket.id];
+    });
 });
 
-setInterval(() => io.emit('update', players), 1000 / 60);
+// ส่งสถานะให้ทุกคน 60 ครั้งต่อวินาที
+setInterval(() => {
+    io.emit('update', players);
+}, 1000 / 60);
+
 http.listen(process.env.PORT || 3000);
