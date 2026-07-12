a48125da0df5d2ec5b6f2cfd828318840927857c
krnlexecutor735-cell <krnlexecutor735@gmail.com>
2026-07-12 17:51:49 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-app.use(express.static('public'));
+const path = require('path');
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 const players = {};
 io.on('connection', (socket) => {
     players[socket.id] = { x: 0, y: 0, os: socket.handshake.query.os || 'Unknown' };
