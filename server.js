92aa809fc828a75104b3e9dee016ca26d7ad4384
krnlexecutor735-cell <krnlexecutor735@gmail.com>
2026-07-12 17:46:25 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
 const express = require('express');
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-app.use(express.static(__dirname));
+app.use(express.static('public'));
 
-let players = {};
+const players = {};
 
 io.on('connection', (socket) => {
-    socket.on('init', (data) => {
-        players[socket.id] = { x: 500, y: 500, os: data.os };
-        io.emit('update', players);
-    });
+  players[socket.id] = { x: 0, y: 0, os: socket.handshake.query.os || 'Unknown' };
 
-    socket.on('move', (pos) => {
-        if (players[socket.id]) {
-            players[socket.id].x = pos.x;
-            players[socket.id].y = pos.y;
-            socket.broadcast.emit('move', { id: socket.id, x: pos.x, y: pos.y });
-        }
-    });
+  socket.on('move', (data) => {
+    if (players[socket.id]) {
+      players[socket.id].x = data.x;
+      players[socket.id].y = data.y;
+    }
+  });
 
-    socket.on('disconnect', () => {
-        delete players[socket.id];
-        io.emit('remove', socket.id);
-    });
+  socket.on('disconnect', () => {
+    delete players[socket.id];
+  });
 });
 
-server.listen(process.env.PORT || 3000);
+setInterval(() => {
+  io.emit('update', players);
+}, 1000 / 60);
+
+http.listen(3000);
