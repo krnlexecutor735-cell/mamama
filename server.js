a8977b51960e69b981496e9f63a65c939455e578
krnlexecutor735-cell <krnlexecutor735@gmail.com>
2026-07-12 17:48:56 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,28 +2,12 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-
 app.use(express.static('public'));
-
 const players = {};
-
 io.on('connection', (socket) => {
-  players[socket.id] = { x: 0, y: 0, os: socket.handshake.query.os || 'Unknown' };
-
-  socket.on('move', (data) => {
-    if (players[socket.id]) {
-      players[socket.id].x = data.x;
-      players[socket.id].y = data.y;
-    }
-  });
-
-  socket.on('disconnect', () => {
-    delete players[socket.id];
-  });
+    players[socket.id] = { x: 0, y: 0, os: socket.handshake.query.os || 'Unknown' };
+    socket.on('move', (data) => { if(players[socket.id]) { players[socket.id].x = data.x; players[socket.id].y = data.y; } });
+    socket.on('disconnect', () => delete players[socket.id]);
 });
-
-setInterval(() => {
-  io.emit('update', players);
-}, 1000 / 60);
-
-http.listen(3000);
+setInterval(() => io.emit('update', players), 1000/60);
+http.listen(process.env.PORT || 3000);
